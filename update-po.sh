6189b126d68577035727c31b8f6df6cc43aca398
Graeme Gott <graeme@gottcode.org>
2013-10-09 18:16:06 -0400

    Update strings.

--- update-po.sh
+++ update-po.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-xgettext --language=C++ --keyword=_ --package-name="xfce4-whiskermenu-plugin" --package-version="1.1.0" `find src -name \*.cpp | sort` -o po/xfce4-whiskermenu-plugin.pot
+xgettext --language=C++ --keyword=_ --package-name="xfce4-whiskermenu-plugin" --package-version="1.2.0" `find src -name \*.cpp | sort` -o po/xfce4-whiskermenu-plugin.pot
 
 for pofile in `find po -name \*.po`;
 do
