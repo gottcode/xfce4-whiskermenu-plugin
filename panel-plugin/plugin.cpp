1caf5fbfa02d032e68a96375706c53756939122e
Graeme Gott <graeme@gottcode.org>
2014-06-25 12:05:14 -0400

    Add name to panel button to allow styling.

--- panel-plugin/plugin.cpp
+++ panel-plugin/plugin.cpp
@@ -123,6 +123,7 @@ Plugin::Plugin(XfcePanelPlugin* plugin) :
 
 	// Create toggle button
 	m_button = xfce_panel_create_toggle_button();
+	gtk_widget_set_name(m_button, "whiskermenu-button");
 	gtk_button_set_relief(GTK_BUTTON(m_button), GTK_RELIEF_NONE);
 	gtk_button_set_focus_on_click(GTK_BUTTON(m_button), false);
 	g_signal_connect_slot(m_button, "button-press-event", &Plugin::button_clicked, this);
