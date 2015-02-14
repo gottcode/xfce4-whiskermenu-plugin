8df414985e3463e54bde74e716711704eebee732
Graeme Gott <graeme@gottcode.org>
2015-02-14 06:32:42 -0500

    Update copyright notice.

--- panel-plugin/window.h
+++ panel-plugin/window.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013, 2014 Graeme Gott <graeme@gottcode.org>
+ * Copyright (C) 2013, 2014, 2015 Graeme Gott <graeme@gottcode.org>
  *
  * This library is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
