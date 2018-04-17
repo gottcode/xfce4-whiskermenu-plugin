fc7709cb3d7d942ba31c7ec3df4fa88c3c2b4212
Graeme Gott <graeme@gottcode.org>
2018-04-16 20:43:13 -0400

    Fix memory leak when recreating window.

--- panel-plugin/page.cpp
+++ panel-plugin/page.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013, 2014, 2015, 2016, 2017 Graeme Gott <graeme@gottcode.org>
+ * Copyright (C) 2013, 2014, 2015, 2016, 2017, 2018 Graeme Gott <graeme@gottcode.org>
  *
  * This library is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -61,6 +61,7 @@ Page::~Page()
 	}
 
 	delete m_view;
+	gtk_widget_destroy(m_widget);
 	g_object_unref(m_widget);
 }
 
