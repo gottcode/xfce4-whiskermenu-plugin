d90a0146735711a0c6610dca4f320143e03a2206
Graeme Gott <graeme@gottcode.org>
2019-07-10 07:52:52 -0400

    Remove workaround for garcon that did not always fix crash.
    This reverts commit 2fb2ec5a0fd031ad7ff377a86e8fd0e06c83ab94.

--- panel-plugin/applications-page.cpp
+++ panel-plugin/applications-page.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013, 2015, 2016, 2017, 2018, 2019 Graeme Gott <graeme@gottcode.org>
+ * Copyright (C) 2013, 2015, 2016, 2017, 2018 Graeme Gott <graeme@gottcode.org>
  *
  * This library is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -160,7 +160,6 @@ void ApplicationsPage::invalidate_applications()
 	if (m_load_status == STATUS_LOADED)
 	{
 		m_load_status = STATUS_INVALID;
-		clear_applications();
 	}
 	else if (m_load_status == STATUS_LOADING)
 	{
@@ -340,15 +339,7 @@ void ApplicationsPage::load_contents()
 	get_window()->set_items();
 	get_window()->set_loaded();
 
-	if (m_load_status == STATUS_LOADING)
-	{
-		m_load_status = STATUS_LOADED;
-	}
-	else
-	{
-		m_load_status = STATUS_INVALID;
-		clear_applications();
-	}
+	m_load_status = (m_load_status == STATUS_LOADING) ? STATUS_LOADED : STATUS_INVALID;
 }
 
 //-----------------------------------------------------------------------------
