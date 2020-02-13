0096dcbc652bb3704b979ee442332e519e6e7466
Graeme Gott <graeme@gottcode.org>
2020-02-13 15:05:30 -0500

    Generate desktop file during compile.

--- po/update-pot.sh
+++ po/update-pot.sh
@@ -2,19 +2,6 @@
 
 
 BASEDIR=../panel-plugin
-WDIR=`pwd`
-
-
-echo -n 'Preparing desktop file...'
-cd ${BASEDIR}
-rm -f whiskermenu.desktop.in
-cp whiskermenu.desktop whiskermenu.desktop.in
-sed -e '/Name\[/ d' \
-	-e '/Comment\[/ d' \
-	-e '/Icon/ d' \
-	-i whiskermenu.desktop.in
-cd ${WDIR}
-echo ' DONE'
 
 
 echo -n 'Extracting messages...'
@@ -22,13 +9,7 @@ echo -n 'Extracting messages...'
 xgettext --from-code=UTF-8 --c++ --keyword=_ --sort-output \
 	--package-name='Whisker Menu' --copyright-holder='Graeme Gott' \
 	--output=xfce4-whiskermenu-plugin.pot ${BASEDIR}/*.cpp ${BASEDIR}/*.h
-xgettext --from-code=UTF-8 --join-existing --sort-output \
+xgettext --from-code=UTF-8 -k --keyword=Name --keyword=Comment --join-existing --sort-output \
 	--package-name='Whisker Menu' --copyright-holder='Graeme Gott' \
 	--output=xfce4-whiskermenu-plugin.pot ${BASEDIR}/*.desktop.in
-echo ' DONE'
-
-
-echo -n 'Cleaning up...'
-cd ${BASEDIR}
-rm -f whiskermenu.desktop.in
 echo ' DONE'
