4bb10c0003fb0eba7c87d64b91b7f8bff5cf3baf
Thomas Bolton <tbolton96@icloud.com>
2026-03-10 11:58:54 +0000

    add favorites menu items to move up and down

--- panel-plugin/page.cpp
+++ panel-plugin/page.cpp
@@ -460,6 +460,24 @@ void Page::create_context_menu(GtkTreePath* path, GdkEvent* event)
 	}
 	else
 	{
+		menuitem = whiskermenu_image_menu_item_new("go-up", _("Move Up"));
+		connect(menuitem, "activate",
+			[this](GtkMenuItem*)
+			{
+				g_assert(m_selected_launcher);
+				m_window->get_favorites()->move_up(m_selected_launcher);
+			});
+		gtk_menu_shell_append(GTK_MENU_SHELL(menu), menuitem);
+
+		menuitem = whiskermenu_image_menu_item_new("go-down", _("Move Down"));
+		connect(menuitem, "activate",
+			[this](GtkMenuItem*)
+			{
+				g_assert(m_selected_launcher);
+				m_window->get_favorites()->move_down(m_selected_launcher);
+			});
+		gtk_menu_shell_append(GTK_MENU_SHELL(menu), menuitem);
+
 		menuitem = whiskermenu_image_menu_item_new("list-remove", _("Remove from Favorites"));
 		connect(menuitem, "activate",
 			[this](GtkMenuItem*)
